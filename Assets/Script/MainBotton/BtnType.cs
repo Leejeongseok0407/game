8f7cbdf8490cdcc8a6534d39856fb261327e2b69
chanhue <hwagchan@gmail.com>
2020-08-19 20:53:53 +0900

    feat : Mainmenu
    메인메뉴의 시작, 설정, 크레딧을 위한 메인메뉴의 화면전환 및 버튼 기능 구현

---
+++ Assets/Script/MainBotton/BtnType.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BtnType : MonoBehaviour
+{
+    public BTNType currentType;
+    public CanvasGroup mainGroup;
+    public CanvasGroup optionGroup;
+    bool isSound;
+
+    public void OnBtnClick() 
+    {
+        switch (currentType) 
+        {
+            case BTNType.Start:
+                UnityEngine.Debug.Log("새게임");
+                break;
+            case BTNType.Option:
+                CanvasGroupOn(optionGroup);
+                CanvasGroupOff(mainGroup);
+                UnityEngine.Debug.Log("설정");
+                break;
+
+            case BTNType.Sound:
+                UnityEngine.Debug.Log("소리");
+                break;
+                //소리사용시 수정해야함
+            case BTNType.Back:
+                CanvasGroupOn(mainGroup);
+                CanvasGroupOff(optionGroup);
+                UnityEngine.Debug.Log("뒤로");
+                break;
+            case BTNType.Credit:
+                UnityEngine.Debug.Log("크레딧");
+                break;
+            case BTNType.Quit:
+                Application.Quit();
+                UnityEngine.Debug.Log("종료");
+                break;
+        }
+    }
+
+    public void CanvasGroupOn(CanvasGroup cg) 
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+
+}
