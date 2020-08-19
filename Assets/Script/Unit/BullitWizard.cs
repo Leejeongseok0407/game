8de5443eecd6b3d71c45759784b4667d5aa43a30
leejeongseok <jsuk10@naver.com>
2020-08-19 22:08:30 +0900

    feat : Explosion()
    폭발 스크립트 제작 몬스터에게 닿으면 폭발하게 구현 하였음.

--- Assets/Script/Unit/BullitWizard.cs
+++ Assets/Script/Unit/BullitWizard.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class BullitWizard : Bullit
 {
+    [SerializeField] float ExplosionRange;
     private void Update()
     {
         MoveBullit();
         RemoveBullit();
+    }
+    void Explosion()
+    {
+        float distance = Vector3.Distance(transform.position, transform.parent.position);
+        if (distance > 1.5f)
+        {
+            // 총알에서 1.0f 반경에 있는 애들을 전부 찾음
+            Collider2D[] hitsCol = Physics2D.OverlapCircleAll(transform.position, ExplosionRange);
+
+            // 몬스터 전부에게 데미지
+            foreach (Collider2D hit in hitsCol)
+            {
+                if (hit.gameObject.tag == "Enemy")
+                {
+                    //hit.gameObject.GetComponent<Mob1>().damage(1);
+                    //몬스터 데미지 입는거 적용
+                }
+            }
+
+            Instantiate(ExplosionParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
