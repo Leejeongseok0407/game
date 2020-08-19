8c6defb67fbb640de1d65f035fc7e9bca223106e
leejeongseok <jsuk10@naver.com>
2020-08-19 20:00:38 +0900

    refactor : Attacker
    각 변수들이 명확한 기능을 나타내도록 이름을 변경함
    한곳에 묶어 통합하여 관리하던 함수들을 각 기능별로 세분화 하여 자식 객체에서 update문에서 작동하도록 구현
    Attacker의 하위 어택부분을 인터페이스화 하여 내려줌 - 각자 다른 기능을 가지고 있기 때문에 공통으로 작업할 필요가 없다고 생각함.

--- Assets/Script/Unit/Parent2/Attacker.cs
+++ Assets/Script/Unit/Parent2/Attacker.cs
@@ -9,69 +9,36 @@ public class Attacker : Unit
     [Tooltip("분당 몇회 타격")]
     [SerializeField] protected float attackSpeed;
 
-    [SerializeField] GameObject bullet = null;
-    private GameObject closeEnemy = null;
-    private List<GameObject> enemyList = new List<GameObject>();
-    private float fTime = 0;
-    private float attackCycle;
+    [SerializeField] protected GameObject bullet = null;
+    protected GameObject closeEnemy = null;
+    protected List<GameObject> enemyList = new List<GameObject>();
+    protected float fTime = 0;
+    protected float attackCycle;
+    protected GameObject target = null;
 
     private void Start()
     {
+        AttackCycleinitializer();
+    }
+    
+
+    protected void AttackCycleinitializer() {
         attackCycle = attackSpeed / 60;
     }
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-
-    void TimeGo()
+    protected void TimeGo()
     {
         fTime += Time.deltaTime;
-        if (enemyList.Count > 0)
-        {
-            TagetOn();
-        }
     }
-    void TagetOn()
+
+    protected void TagetSet()
     {
         GameObject target = enemyList[0];
-        Attack();
-        if (gameObject.tag == "Wizard")
-        {
-            if (target != null && fTime > attackCycle)
-            {
-                fTime = 0.0f;
-                //불릿을 생성
-                var aBullet = Instantiate(bullet, transform.position, Quaternion.identity, transform);
-                aBullet.GetComponent<BullitWizard>().SetTargetPosition((target.transform.position - transform.position).normalized);
-                aBullet.transform.localScale = new Vector3(0.5f, 0.5f);
-            }
-        }
-
-        else if (gameObject.tag == "Ranger")
-        {
-            if (target != null && fTime > attackCycle)
-            {
-                fTime = 0.0f;
-                //불릿을 생성함.
-                var aBullet = Instantiate(bullet, transform.position, Quaternion.identity, transform);
-                //방향 벡터 생성
-                Vector3 dir = (target.transform.position - transform.position).normalized;
-                //앵글을 생성하여 회전각을 생성
-                float angle = Vector2.SignedAngle(Vector2.down, dir);
-                Quaternion qut = new Quaternion();
-                qut.eulerAngles = new Vector3(0, 0, angle);
-                aBullet.transform.rotation = qut;
-                aBullet.transform.position += dir * 1.0f;
-            }
-        }
+        
     }
 
-    void Attack()
-    {
+    interface Attack { }
 
-    }
     //트리거 범위에 몬스터 들어올 경우 몬스터를 리스트에 삽입한다.
     private void OnTriggerEnter2D(Collider2D collision)
     {
