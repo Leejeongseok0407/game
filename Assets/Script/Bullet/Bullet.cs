3076c8a7dbed51860acdf9ba746f4f7520f3a94c
이정석 <jsuk10@naver.com>
2020-08-21 22:00:37 +0900

    fix : bullet
    불릿이 회전하면서 앞으로 나아가려 했기 때문에 크게 원을 돌며 방향을 찾아갔으나 이제는 그냥 앞으로 가고 회전값만 주어서 타겟을 찾아가게 구현함.

--- Assets/Script/Bullet/Bullet.cs
+++ Assets/Script/Bullet/Bullet.cs
@@ -12,22 +12,20 @@ public class Bullet : MonoBehaviour
 
     //총알 움직이는 함수
     protected void MoveBullet() {
-        Vector3 dir = Vector3.Normalize(target.transform.position - transform.position); 
-        transform.Translate(dir * Time.deltaTime * bullitSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * bullitSpeed);
         Debug.Log("moveBullet");
     }
 
     protected void LookAtBullet()
     {
-        Vector3 lookDir = target.transform.position;
-        lookDir.z = 0;
-        transform.rotation.SetLookRotation(lookDir);
+        Vector3 lookDir = target.transform.position- transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
     }
 
-    //불릿이 일정거리 벗어나면 없어지는 함수
     protected void ReturnBullet() 
     {
-        // 나와 부모의 사이가 일정거리(1.5f) 도달하면 삭제
         float distance = Vector3.Distance(transform.position, target.transform.position);
         Debug.Log(distance);
         if (distance < hitRange)
