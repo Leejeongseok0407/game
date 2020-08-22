c28c960920b330b171a601e77dcb6d60118b84b8
jisuk <zeya9643@naver.com>
2020-08-23 03:05:02 +0900

    feat: MapManager
    맵관리에 필요한 맵매니져 구현

---
+++ Assets/Script/Manager/MapManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public static MapManager Instance;
+    bool isStageChanged;
+    int curStage;
+    const int mapHeightMax = 8;
+    const int mapWidthMax = 12;
+    int[,] gridStatus = new int[mapHeightMax, mapWidthMax];
+    void Awake()
+    {
+        Instance = this;
+        isStageChanged = false;
+        curStage = 0;
+        SetStage();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isStageChanged)
+        {
+            curStage++;
+            SetStage();
+        }
+        
+    }
+
+    void SetStage()
+    {
+        SetMapMatrix();
+    }
+
+    void SetMapMatrix()
+    {
+
+    }
+}
