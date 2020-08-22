cea0765efb96b4b5830caee7b42bd921b824ca73
이정석 <jsuk10@naver.com>
2020-08-23 03:14:27 +0900

    feat : SlotInventory
    슬롯 인벤토리 구현함.
    자식들의 수만큼 for문을 구현 - 자식의 수만큼 업데이트 해줘야 해서
    slot을 unitInventory와 비교 해서 수보다 적으면 slot에서 변경 시키는 함수 적용 - 인벤토리 업데이트
    아니면 null보냄 - 없다는걸 암시
    이후 slots리스트에 slot추가.

---
+++ Assets/Script/SlotInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotInventory : MonoBehaviour
+{
+    [SerializeField] UnitContainerManager UnitContainer;
+    [SerializeField] Transform slotRoot;
+    [SerializeField] List<Slot> slots;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < slotRoot.childCount; i++)
+        {
+            var slot = slotRoot.GetChild(i).GetComponent<Slot>();
+            if (i < UnitContainer.unitInventoryArry.Count)
+                slot.SetUnit(UnitContainer.unitInventoryArry[i]);
+            else
+                slot.SetUnit(null);
+            slots.Add(slot);
+        }
+    }
+
+}
