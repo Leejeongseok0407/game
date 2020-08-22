ecdb8e34de6149368c73f810070aac4beb031221
이정석 <jsuk10@naver.com>
2020-08-23 03:09:49 +0900

    feat : slot
    슬롯을 만들었음. Null을 받으면 이름과 이미지를 null
    unit받을 경우 이름과 이미지를 맞춰줌

---
+++ Assets/Script/Slot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    [SerializeField] Image image;
+
+    public void SetUnit(Unit unit) {
+
+        if (unit == null)
+        {
+            Debug.Log("Null");
+            image = null;
+            gameObject.name = "Null";
+        }
+        else {
+            image.sprite = unit.GetComponentInChildren<SpriteRenderer>().sprite;
+            gameObject.name = unit.name;
+        }
+    }
+
+}
