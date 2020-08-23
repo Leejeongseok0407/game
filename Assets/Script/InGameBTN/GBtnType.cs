59afc6f13791f36b20dbaf268e6c172c36a978e3
DESKTOP-04215QM\USER <hwagchan@gmail.com>
2020-08-24 00:33:32 +0900

    feat : InGameUI
    인게임 화면에서의 UI구현

---
+++ Assets/Script/InGameBTN/GBtnType.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GBtnType : MonoBehaviour
+{
+
+    public CanvasGroup GmainGroup;
+    public CanvasGroup GoptionGroup;
+    bool isSound;
+    public AudioSource musicsource;
+    public void OnGBtnClick(string GBTN)
+    {
+        switch (GBTN)
+        {
+            case "Option":
+                CanvasGroupOn(GoptionGroup);
+                CanvasGroupOff(GmainGroup);
+                break;
+
+            case "Sound":
+                if (isSound)
+                {
+                    musicsource.volume = 0f;
+                    Debug.Log("soundOFF");
+                }
+                else
+                {
+                    musicsource.volume = 1f;
+                    Debug.Log("SoundOn");
+                }
+                isSound = !isSound;
+                break;
+
+            case "Back":
+                CanvasGroupOn(GmainGroup);
+                CanvasGroupOff(GoptionGroup);
+                break;
+
+            case "Quit":
+
+                break;
+        }
+    }
+
+    public void CanvasGroupOn(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+}
---
+++ Assets/Script/InGameBTN/GUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GBTNType
+{
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+public class GUI : MonoBehaviour
+{
+
+}
