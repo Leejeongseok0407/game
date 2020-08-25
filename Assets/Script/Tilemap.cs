95381b19a93611c60511a5dac85c18713b04d03f
jisuk <zeya9643@naver.com>
2020-08-25 10:37:04 +0900

    feat: tilemap
    임시 타일맵 스크립트 추가

---
+++ Assets/Script/Tilemap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilemap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+
+
+        Vector3Int cellPosition = gridLayout.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //transform.position = gridLayout.CellToWorld(cellPosition);
+        Debug.Log(gridLayout.CellToWorld(cellPosition));
+        //Debug.Log(transform.position);
+    }
+}
