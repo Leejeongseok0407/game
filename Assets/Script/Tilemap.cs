c3b6132f44fa86679f686b2567898235e420813c
jisuk <zeya9643@naver.com>
2020-08-25 15:47:29 +0900

    refactor: GetTilePositionByIndex
    동일 함수를 mapmanger로 옮김

--- Assets/Script/Tilemap.cs
+++ Assets/Script/Tilemap.cs
@@ -4,22 +4,4 @@ using UnityEngine;
 
 public class Tilemap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
- 
-    }
-
-    public Vector3 GetTilePositionByIndex(int row, int col)
-    {
-        float tilePositionX = transform.position.x + 5 - row;
-        float tilePositionY = transform.position.y + col - 10;
-        return new Vector3(tilePositionX, tilePositionY, 0);
-    }
 }
