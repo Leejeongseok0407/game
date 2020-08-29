22d51e94630df8f9f5134ba4894c126835531b23
DESKTOP-04215QM\USER <hwagchan@gmail.com>
2020-08-29 22:12:24 +0900

    feat : credit change
    크레딧 일정 시간 후 메인으로 돌아오도록 구현

---
+++ Assets/Script/MainBotton/CreditQ.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditQ : MonoBehaviour
+{
+    public CanvasGroup mainGroup;
+    public CanvasGroup creditGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Scenechange", 10f);
+        CanvasGroupOn(mainGroup);
+        CanvasGroupOff(creditGroup);
+    }
+
+    private void Scenechange()
+    {
+        CanvasGroupOn(mainGroup);
+        CanvasGroupOff(creditGroup);
+    }
+    public void CanvasGroupOn(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+    // Update is called once per frame
+}
